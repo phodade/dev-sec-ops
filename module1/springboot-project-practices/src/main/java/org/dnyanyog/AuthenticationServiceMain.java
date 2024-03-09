964f84fe9d222194107b044d1cc4a0d2999dd35a
phodade <pratikshahodade@gmail.com>
2024-03-09 12:13:35 +0530

    feat: Implement authentication-service

---
+++ module1/springboot-project-practices/src/main/java/org/dnyanyog/AuthenticationServiceMain.java
@@ -0,0 +1,14 @@
+package org.dnyanyog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.time.Duration;
+
+@SpringBootApplication 
+public class AuthenticationServiceMain {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServiceMain.class, args);
+	}
+
+}
